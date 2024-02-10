dd3857c2357ddce47f53144bde9457239dc8ad79
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-02-10 11:48:27 +0100

    add animations to the card

--- onBoarding/src/app/app.component.ts
+++ onBoarding/src/app/app.component.ts
@@ -5,12 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 // import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
+
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Sprint 5';
